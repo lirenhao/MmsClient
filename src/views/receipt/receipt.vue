511106d525338f55fa664a0a1f100b520a946665
lirenhao <renhao.li@bjyada.com>
2018-05-24 14:31:10 +0800

    回执列表

--- src/views/receipt/receipt.vue
+++ src/views/receipt/receipt.vue
@@ -6,11 +6,11 @@
       <!--<cell-form-preview list="list"></cell-form-preview>-->
     </group>
     <install v-if="info.eventsId === '0' || info.eventsId === '1'"
-             :handler="submit" :info="info"/>
+             :submit="submit" :save="save" :info="info" :init="{}"/>
     <uninstall v-if="info.eventsId === '2'"
-               :handler="submit" :info="info"/>
+               :submit="submit" :save="save" :info="info" :init="{}"/>
     <inspect v-if="info.eventsId === '3' || info.eventsId === '4' || info.eventsId === '5'"
-             :handler="submit" :info="info"/>
+             :submit="submit" :save="save" :info="info" :init="{}"/>
   </div>
 </template>
 
@@ -19,24 +19,35 @@
   import Install from '../../components/Install'
   import Uninstall from '../../components/Uninstall'
   import Inspect from '../../components/Inspect'
-  import api from '../../api'
   import * as consts from '../../constants'
+  import api from '../../api'
 
   export default {
     name: "receipt",
     components: {
       XHeader, Group, Cell, CellFormPreview, Install, Uninstall, Inspect
     },
-    props: ['info', 'type'],
+    props: ['info', 'init'],
     data: function () {
       return {
         title: consts.eventType[this.info.eventsId]
       }
     },
     methods: {
-      submit(value) {
-        console.log(value)
+      submit(params, images, devNos) {
+        params.workId = this.info.workId
+        params.termNo = this.info.termNo
+        params.eventsId = this.info.eventsId
+        params.addr = this.info.addr
+        api.workReceipt(params, images, devNos)
       },
+      save(params, images, devNos) {
+        params.workId = this.info.workId
+        params.termNo = this.info.termNo
+        params.eventsId = this.info.eventsId
+        params.addr = this.info.addr
+        api.saveReceipt(params, images, devNos)
+      }
     }
   }
 </script>
